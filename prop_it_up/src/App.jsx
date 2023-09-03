1c447a84639b3d9ca0f4613cba6b4e2766400f0f
esohi <esohi@proton.me>
2023-09-04 00:51:11 +0200

    second commit - putting_it_together

--- prop_it_up/src/App.jsx
+++ prop_it_up/src/App.jsx
@@ -6,11 +6,13 @@ function App() {
   return (
     <>
       <div className='persons'>
-        <PersonCard firstName={'John'} lastName={'Smith'} age={'88'} hairColor={'Brown'} />
-        <PersonCard firstName={'Millard'} lastName={'Fillmore'} age={'50'} hairColor={'Brown'} />
-        <PersonCard firstName={'Smith'} lastName={'Maria'} age={'62'} hairColor={'Brown'} />
-        <PersonCard firstName={'Jane'} lastName={'Doe'} age={'45'} hairColor={'Black'} />
+        <PersonCard firstName={'John'} lastName={'Smith'} age={88} hairColor={'Brown'} />
+        <PersonCard firstName={'Millard'} lastName={'Fillmore'} age={50} hairColor={'Brown'} />
+        <PersonCard firstName={'Smith'} lastName={'Maria'} age={62} hairColor={'Brown'} />
+        <PersonCard firstName={'Jane'} lastName={'Doe'} age={45} hairColor={'Black'} />
       </div>
+
+
     </>
   )
 }
--- prop_it_up/src/components/PersonCard.jsx
+++ prop_it_up/src/components/PersonCard.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, {useState} from 'react';
 
-const PersonCard = (props) => {
-    const {firstName, lastName, age, hairColor} = props;
+ 
+const PersonCard = props => {
+
+    const { firstName, lastName, age, hairColor } = props;
+    const [stateAge, setStateAge] = useState(age);
+
+    const handleClick = () => {
+        setStateAge(stateAge + 1);
+    }
+
     return (
-        <>
+        <div>
             <h1>{lastName}, {firstName}</h1>
-                <p>Age: {age}</p>
-                <p>Hair Color: {hairColor}</p>
-        </>
+            <p>Age: {stateAge}</p>
+            <p>Hair Color: {hairColor}</p>
+            <button onClick={handleClick}>Birthday Button for {firstName} {lastName}</button>
+        </div>
     );
 
 }
